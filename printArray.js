4789cb4fb58c89fb5671470eedfd0dfaccb3b473
Joelson Junior <53396470+joelsonejr@users.noreply.github.com>
2022-03-29 23:46:46 -0300

    Program to print an array in reverse order

---
+++ printArray.js
@@ -0,0 +1,28 @@
+
+function processData(input) {
+
+    let inputToArray = input.split('\n'); 
+
+    let arrayLength = inputToArray[0];
+
+    let inputString = "";
+    
+    for (let i = 1; i <= arrayLength; i++){
+        
+            inputString += array[i];
+
+}
+
+let lastItem = arrayLength - 1; 
+
+function printArray(inputString) {
+    
+    let outputArray = ""
+    
+    for(lastItem ; lastItem >= 0; lastItem--){
+        outputArray += (jon[lastItem] + ' ');
+    }
+
+    console.log(outputArray);
+}
+
